4446cee1230b7c528abc669d54705817fd4b4739
zydemo <564359804@163.com>
2019-05-16 13:31:44 +0800

    设置static和media/JS/CSS

--- dailyfresh/dailyfresh/settings.py
+++ dailyfresh/dailyfresh/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static'),
+]
+MEDIA_ROOT = os.path.join(BASE_DIR,'static/media').replace('\\','/')
